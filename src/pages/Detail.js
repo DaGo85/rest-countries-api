ee60d4ad1b226936db38e914fc883a05f6d6c30e
DaGo85 <davidgoe85@gmail.com>
2022-02-26 11:46:42 +0100

    finished project

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -8,6 +8,15 @@ function Detail() {
 
   const [borderData, setBorderData] = useState([])
   const [data, setData] = useState([])
+  const [languagesString, setLanguagesString] = useState("")
+
+  const getLanguages = (data) => {
+    let trimmedString = ""
+    data[0].languages.map((language) => {
+      trimmedString += `${language.name}, `
+    })
+    setLanguagesString(trimmedString.slice(0, -2))
+  }
 
   useEffect(() => {
     axios
@@ -16,61 +25,93 @@ function Detail() {
       )
       .then((response) => {
         setData(response.data)
-        console.log("hallowelt" + JSON.stringify(response.data))
+        getLanguages(response.data)
         const borderArray = response.data[0].borders
         axios
           .get(`https://restcountries.com/v3.1/alpha?codes=${borderArray}`)
           .then((response) => {
             setBorderData(response.data)
-            console.log("first" + JSON.stringify(response.data[0].borders))
           })
-        console.log("test1" + JSON.stringify(response.data))
       })
   }, [path])
 
   useEffect(() => {}, [data[0]])
 
   return (
-    <>
-      <Link to="/">
-        <button>
+    <div className="detail">
+      <Link className="remove-deco" to="/">
+        <button className="back-button">
           <img src={Arrow} alt="Arrow" />
           Back
         </button>
       </Link>
       {data[0] && (
-        <>
+        <div className="detail-body">
           <img src={data[0].flags.svg} alt="country-flag" />
-          <div>
-            <h2>{data[0].name}</h2>
-            <h5>Native Name: {data[0].nativeName}</h5>
-            <h5>
-              Population:{" "}
-              {data[0].population
-                .toString()
-                .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.")}
-            </h5>
-            <h5>Region: {data[0].region}</h5>
-            <h5>Sub Region: {data[0].subregion}</h5>
-            <h5>Capital: {data[0].capital}</h5>
-            <h5>Top Level Domain: {data[0].topLevelDomain}</h5>
-            <h5>Currencies: {data[0].currencies[0].name}</h5>
-            <h5>Languages: {data[0].languages[0].name}</h5>
-            <div>
-              Border Countries:{" "}
-              {borderData &&
-                borderData.map((borderCountry) => {
-                  return (
-                    <div key={borderCountry.name.common}>
-                      {borderCountry.name.common}
-                    </div>
-                  )
-                })}
+          <div className="detail-body-text">
+            <div className="detail-body-control">
+              <div className="main-detail-text">
+                <h2>{data[0].name}</h2>
+                <h5>
+                  <b>Native Name: </b> {data[0].nativeName}
+                </h5>
+                <h5>
+                  <b>Population: </b>
+                  {data[0].population
+                    .toString()
+                    .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.")}
+                </h5>
+                <h5>
+                  <b>Region: </b>
+                  {data[0].region}
+                </h5>
+                <h5>
+                  <b>Sub Region: </b> {data[0].subregion}
+                </h5>
+                <h5>
+                  <b>Capital: </b> {data[0].capital}
+                </h5>
+              </div>
+              <div className="side-detail-text">
+                <h5>
+                  <b>Top Level Domain: </b> {data[0].topLevelDomain}
+                </h5>
+                <h5>
+                  <b>Currencies: </b> {data[0].currencies[0].name}
+                </h5>
+
+                <h5>
+                  <b>Languages: </b> {languagesString && languagesString}
+                </h5>
+              </div>
+            </div>
+            <div className="border-countries">
+              <h5>
+                <b>Border Countries:</b>
+              </h5>
+              <div className="border-button-bar">
+                {borderData &&
+                  borderData.map((borderCountry) => {
+                    return (
+                      <Link
+                        className="remove-deco"
+                        to={`/${borderCountry.name.common}`}
+                      >
+                        <button
+                          className="border-button"
+                          key={borderCountry.name.common}
+                        >
+                          {borderCountry.name.common}
+                        </button>
+                      </Link>
+                    )
+                  })}
+              </div>
             </div>
           </div>
-        </>
+        </div>
       )}
-    </>
+    </div>
   )
 }
 
